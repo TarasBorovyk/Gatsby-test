cd0c5f508b080158a77930ad5998add7aafcdcc1
Tapacuk <ken4ik.gamer@gmail.com>
2020-07-30 13:33:51 +0300

    Set gatsby cloud keys

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,33 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-let env = process.env.NODE_ENV || 'development';
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
-// This adds dotenv (for storing environment variables) to gatsby
-require('dotenv').config({path: `./.env.${env}`});
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+}
+
+// if you want to use the preview API please define
+// CONTENTFUL_HOST in your environment config
+// the `host` property should map to `preview.contentful.com`
+// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = process.env.CONTENTFUL_HOST
+}
+
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    'Contentful spaceId and the access token need to be provided.'
+  )
+}
 
 module.exports = {
-  /* Your site config here */
   plugins: [
     {
       resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-      },
-    }
+      options: contentfulConfig,
+    },
   ],
 }
